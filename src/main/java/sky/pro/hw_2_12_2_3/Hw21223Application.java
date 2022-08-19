4210e6a21bd4b59cc62314c9a7e5d3a9d545120e
ElenaR <llinusik@mail.ru>
2022-08-19 13:35:01 +0300

    Initial commit

---
+++ src/main/java/sky/pro/hw_2_12_2_3/Hw21223Application.java
@@ -0,0 +1,13 @@
+package sky.pro.hw_2_12_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw21223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw21223Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/hw_2_12_2_3/Hw21223ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hw_2_12_2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw21223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
